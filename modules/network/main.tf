fde9800a2a573f7874212d479ac59b3574c130ea
vanshhthakral <vanshnrw@gmail.com>
2026-08-26 23:54:42 +0530

    Restrict SSH ingress to admin IP

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -74,7 +74,7 @@ resource "aws_security_group" "web" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["106.219.149.250/32"]
   }
 
   egress {
