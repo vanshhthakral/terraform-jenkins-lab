346645bfaf7987dfc2cc92e29d6cd1fd5356e5a9
vanshhthakral <vanshnrw@gmail.com>
2026-08-31 21:23:04 +0530

    Restrict DB security group egress

--- modules/network/main.tf
+++ modules/network/main.tf
@@ -12,7 +12,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidr
   availability_zone       = var.availability_zone
-  map_public_ip_on_launch = true
+  map_public_ip_on_launch = false
 
   tags = {
     Name = "advanced-iac-public-subnet"
@@ -66,7 +66,7 @@ resource "aws_security_group" "web" {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["106.219.149.250/32"]
   }
 
   ingress {
@@ -81,7 +81,7 @@ resource "aws_security_group" "web" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["106.219.149.250/32"]
   }
 
   tags = {
@@ -103,10 +103,11 @@ resource "aws_security_group" "db" {
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    description     = "Allow DB to communicate with web tier"
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    security_groups = [aws_security_group.web.id]
   }
 
   tags = {
